6c2e02dcc74c980cdafc867403e9ec3e1ce6f6bd
Aleksy <aleksy.geborek@gmail.com>
2024-10-18 14:45:18 +0200

    first tailwindcss project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js}'],
+	theme: {
+		fontFamily: {
+			YoungSerif: ['YoungSerif', 'sans-serif'],
+			Outfit: ['Outfit', 'sans-serif'],
+		},
+		extend: {},
+	},
+	plugins: [],
+};
